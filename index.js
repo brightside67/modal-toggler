833b5aceecdde03bc8bb4ff3ac0b7edbb9d35afb
brightside67 <mobrika@list.ru>
2018-02-17 11:39:33 +0700

    added fix to npm modules

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-"use strict";modal.exports=function(e){var t={modalClass:e.modalClass||!1,toggleClass:e.toggleClass||!1,backgroundNode:e.backgroundNode||!1,backgroundClass:e.backgroundClass||!1},a=t.modalClass,l=document.querySelector(e.backgroundNode),o=e.backgroundClass,s=document.querySelectorAll("[data-modal-open]"),r=document.querySelectorAll("[data-modal-close]"),d=document.querySelectorAll("[data-modal-toggle]"),c=s.length,n=r.length,u=d.length;if(c)for(var g=0;g<c;g++)s[g].addEventListener("click",function(e){e.preventDefault();var t=e.target.dataset.modalOpen;document.querySelector(t).classList.add(a),l&&l.classList.add(o)});if(n)for(var i=0;i<n;i++)r[i].addEventListener("click",function(e){e.preventDefault();var t=e.target.dataset.modalClose;document.querySelector(t).classList.remove(a),l&&l.classList.remove(o)});if(u)for(var m=0;m<u;m++)d[m].addEventListener("click",function(e){e.preventDefault();var a=e.target.dataset.modalToggle;document.querySelector(a).classList.toggle(t.toggleClass)})};
+"use strict";
+
+modal.exports = function modalToggler(options) {
+  var optionsBox = {
+    modalClass: options.modalClass || false,
+    toggleClass: options.toggleClass || false,
+    backgroundNode: options.backgroundNode || false,
+    backgroundClass: options.backgroundClass || false
+  };
+  var modalClass = optionsBox.modalClass;
+  var backgroundNode = document.querySelector(options.backgroundNode);
+  var backgroundClass = options.backgroundClass;
+  var modalsArr = document.querySelectorAll("[data-modal-open]");
+  var closeArr = document.querySelectorAll("[data-modal-close]");
+  var toggleArr = document.querySelectorAll("[data-modal-toggle]");
+  var modalsArrLength = modalsArr.length;
+  var closeArrLength = closeArr.length;
+  var toggleArrLength = toggleArr.length;
+
+  if (modalsArrLength) {
+    for (var i = 0; i < modalsArrLength; i++) {
+      modalsArr[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        var targetNode = e.target.dataset.modalOpen;
+        document.querySelector(targetNode).classList.add(modalClass);
+        if (backgroundNode) {
+          backgroundNode.classList.add(backgroundClass);
+        }
+      });
+    }
+  }
+
+  if (closeArrLength) {
+    for (var _i = 0; _i < closeArrLength; _i++) {
+      closeArr[_i].addEventListener("click", function(e) {
+        e.preventDefault();
+        var targetNode = e.target.dataset.modalClose;
+        document.querySelector(targetNode).classList.remove(modalClass);
+        if (backgroundNode) {
+          backgroundNode.classList.remove(backgroundClass);
+        }
+      });
+    }
+  }
+
+  if (toggleArrLength) {
+    for (var _i2 = 0; _i2 < toggleArrLength; _i2++) {
+      toggleArr[_i2].addEventListener("click", function(e) {
+        e.preventDefault();
+        var targetNode = e.target.dataset.modalToggle;
+        document
+          .querySelector(targetNode)
+          .classList.toggle(optionsBox.toggleClass);
+      });
+    }
+  }
+};
